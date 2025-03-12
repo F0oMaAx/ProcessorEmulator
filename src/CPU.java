617048dfe1521ae96d8594b8be4da81a9e0ab0e9
F0oMaAx <foomaax@gmail.com>
2025-03-12 10:45:31 +0500

    Add missed CPU

---
+++ src/CPU.java
@@ -0,0 +1,16 @@
+public class CPU {
+    private final Registers registers = new Registers();
+    private final ALU alu = new ALU();
+
+    public CPU(int memorySize) {
+        Memory memory = new Memory(memorySize); // 1 MB памяти
+    }
+
+    public void executeInstruction(String instruction) {
+        alu.execute(instruction);
+    }
+
+    public void dumpRegisters() {
+        registers.dumpRegisters();
+    }
+}
